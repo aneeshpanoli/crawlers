331f729633d0ff2f3ac73a4aa6c548e07cb70686
Aneesh Panoli <aneeshpanoli@gmail.com>
2020-03-28 19:36:06 -0700

    Added functionality
    - added a class to extract email address and links for the 'Resources' column
    - removed duplicates
    - output CSV now should be ready to push to the database
    - there are no accompanying images for these projects
    
    pending:
    - the country column is pretty much garbage, the site only lists if the project is local or remote
    - we could use some machine learning here to classify the text to countries based on identifying information - or manually label the text
    - need to crawl the project homepage to extract images

--- scrapy_helpwithcovid.py
+++ scrapy_helpwithcovid.py
@@ -1,50 +1,98 @@
+
+'''
+A Scrapy spider class to crawl and extract social hackathon projects from helpwithcovid.com written for ccivictechhub.org
+'''
+__author__= "Aneesh Panoli"
+
+
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
 from collections import defaultdict
 import pandas as pd
-import country
-import tools
 from pathlib import Path
 import os
+import re
 
 class EventsSpider(scrapy.Spider):
     name = 'events'
     start_urls = []
 
-    # 12 pages of projects
+    # 15 pages of projects
     for i in range(1, 16):
         start_urls.append('https://helpwithcovid.com/projects/p/{}'.format(i))
 
     def __init__(self):
         self.data = defaultdict(list)
         # path to the data folder
-        self.data_path = Path(Path(__file__).resolve().parent, "output")
+        self.data_path = Path(Path(__file__).resolve().parent, "csv")
 
     def parse(self, response):
-        if 'helpwithcovid' in response.url:
-            self.helpwithcovid(response)
+        self.helpwithcovid(response)
         df = pd.DataFrame.from_dict(self.data, orient='index')
-        df.transpose().to_csv(os.path.join(self.data_path, 'helpwithcovid_events.csv'))
+        df.transpose().to_csv(os.path.join(self.data_path, 'helpwithcovid.csv'), header=True, index=False)
+
 
     def helpwithcovid(self, response):
         helpwithcovid_pattern = {
-            'names':'//div[@class="text-sm leading-5 font-medium text-indigo-600 truncate"]/a/text()',
-            'elinks':'//div[@class="text-sm leading-5 font-medium text-indigo-600 truncate"]/a/@href',
-            'country_id':'//div[@class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0"]/text()',
-            'summary':'//div[@class="text-sm leading-5 text-gray-500"]/p/text()',
-            'image':'//div[@class="noimageshere"]'
+            'GroupName':'//div[@class="text-sm leading-5 font-medium text-indigo-600 truncate"]/a/text()',
+            'Resources':'//div[@class="text-sm leading-5 font-medium text-indigo-600 truncate"]/a/@href',
+            'CountryName':'//div[@class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0"]/text()[2]',
+            'Summary':'//div[@class="text-sm leading-5 text-gray-500"]/p[1]/text()[1]',
+            'ImageUrl':'//div[@class="noimageshere"]'
         }
         for key, value in helpwithcovid_pattern.items():
             parsed = response.xpath(value).extract()
-            if key == 'elinks':
+            if key == 'Resources':
                 parsed = ['https://helpwithcovid.com'+i for i in parsed]
-            elif key == 'country_id':
-                parsed = [i.rstrip() for i in parsed]
             self.data[key].extend(parsed)
 
+
+class DemoContactSpider(scrapy.Spider):
+
+    name = 'democontact'
+    start_urls = pd.read_csv(os.path.join(Path(Path(__file__).resolve().parent, "csv"), 'helpwithcovid.csv'))['Resources'].tolist()
+    # start_urls = ['https://helpwithcovid.com/projects/309-gift-card-bank']
+
+    def __init__(self):
+        self.df = pd.read_csv(os.path.join(Path(Path(__file__).resolve().parent, "csv"), 'helpwithcovid.csv'))
+
+        self.data = []
+        # self.text_list = []
+        self.data_path = Path(Path(__file__).resolve().parent, "csv")
+
+    def parse(self, response):
+        self.democontact(response)
+        # print(len(self.data), len(self.df))
+        if len(self.data) == len(self.df):
+            # df_text = pd.DataFrame()
+            # df_text['text'] = self.text_list
+            # df_text.to_csv('text.csv', header=True, index=False)
+            self.df['Resources'] = self.data
+            self.df.to_csv(os.path.join(self.data_path, 'helpwithcovid_clean.csv'), header=True, index=False)
+            print("******helpwithcovid_clean.csv has been saved to {}!".format(self.data_path))
+
+    def democontact(self, response):
+
+        text_links = self.link_from_text(', '.join([i for i in response.xpath("//dl//text()").extract() if i]))
+        self.data.append('|||'.join([i for i in [text_links, response.url] if i]))
+
+    def link_from_text(self, text):
+        # self.text_list.append(text)
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+        emails = re.findall('[\w\.-]+@[\w\.-]+', text)
+        return '|||'.join([i.strip(',') for i in urls+emails if i])
+
+@defer.inlineCallbacks
+def crawl(spyder):
+    yield spyder.crawl(EventsSpider)
+    yield spyder.crawl(DemoContactSpider)
+    reactor.stop()
+
+
 if __name__ == '__main__':
-    spyder = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-    spyder.crawl(EventsSpider)
-    spyder.start()
+    configure_logging()
+    spyder = CrawlerRunner()
+    crawl(spyder)
+    reactor.run()
