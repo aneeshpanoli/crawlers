7976361fe315fb270657046daee65b5d3bcd9aa8
Guillaume Duc <isotopian@msn.com>
2020-04-07 23:26:30 +0200

    Extract more frequent words from the story as tag

--- wirvsvirus/DevPostCrawler/pipelines.py
+++ wirvsvirus/DevPostCrawler/pipelines.py
@@ -4,12 +4,15 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+import spacy
+from collections import Counter
 
 
 class DevPostCrawlerPipeline(object):
 
     def __init__(self):
         self.file = open('items.jl', 'w')
+        self.nlp = spacy.load("en_core_web_sm")
 
     def open_spider(self, spider):
         pass
@@ -18,6 +21,14 @@ class DevPostCrawlerPipeline(object):
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
+        key_words = []
+        text = item['storyText'][0].rstrip()
+        doc = self.nlp(text)
+        for chunk in doc.noun_chunks:
+            key_words.append(chunk.root.text)
+        dict_item = dict(item)
+        dict_item['popular_word'] = [a.title() for a, _
+                                     in Counter(key_words).most_common(3)]
+        line = json.dumps(dict_item) + "\n"
         self.file.write(line)
         return item
