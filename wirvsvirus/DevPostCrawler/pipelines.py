3fd9387957a473ac49af6617e5e15da2d2c75cf3
Guillaume Duc <isotopian@msn.com>
2020-04-08 03:48:43 +0200

    DEVPOST: add subtitle translation and language detection

--- wirvsvirus/DevPostCrawler/pipelines.py
+++ wirvsvirus/DevPostCrawler/pipelines.py
@@ -6,6 +6,8 @@
 import json
 import spacy
 from collections import Counter
+from googletrans import Translator
+from googletrans import LANGUAGES
 
 
 class DevPostCrawlerPipeline(object):
@@ -13,6 +15,7 @@ class DevPostCrawlerPipeline(object):
     def __init__(self):
         self.file = open('items.jl', 'w')
         self.nlp = spacy.load("en_core_web_sm")
+        self.translator = Translator()
 
     def open_spider(self, spider):
         pass
@@ -21,6 +24,8 @@ class DevPostCrawlerPipeline(object):
         self.file.close()
 
     def process_item(self, item, spider):
+
+        # Extract most frequent words
         key_words = []
         text = item['storyText'][0].rstrip()
         doc = self.nlp(text)
@@ -29,6 +34,15 @@ class DevPostCrawlerPipeline(object):
         dict_item = dict(item)
         dict_item['popular_word'] = [a.title() for a, _
                                      in Counter(key_words).most_common(3)]
+
+        # Detect the subtitle language and translate it to english
+        a = self.translator.translate(item['subtitle'][0])
+        dict_item['language'] = LANGUAGES[a.src]
+        if a.src != 'en':
+            dict_item['en_subtitle'] = a.text
+        else:
+            dict_item['en_subtitle'] = ''
+
         line = json.dumps(dict_item) + "\n"
         self.file.write(line)
         return item
