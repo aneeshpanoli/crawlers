076d2788c0b1803d2df0a83f2d3a115f09a6e501
Niko Schmuck <n.schmuck@nava.de>
2020-04-17 09:37:25 +0200

    HACK subtext translation.

--- devpost/DevPostCrawler/pipelines.py
+++ devpost/DevPostCrawler/pipelines.py
@@ -4,6 +4,7 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+import logging
 from DevPostCrawler.language_processing import LanguageProcessing
 
 
@@ -28,12 +29,17 @@ class DevPostCrawlerPipeline(object):
         item['language'] = language
 
         # Translate to english if necessary
-        if language != 'english' or language != 'en' or score < 0.85:
-            item['storyText'] = self.lp.ggl_translate(story_text, language)
-            item['subtitle'] = self.lp.ggl_translate(subtitle, language)
-        else:
+        logging.debug('Detected language %s with score %.3f', language, score)
+        if language == 'english' or language == 'en':  # and score > 0.85:
             item['storyText'] = story_text
+        else:
+            item['storyText'] = self.lp.ggl_translate(story_text, language)
+
+        (score_st, language_st) = self.lp.spc_detect_language(subtext)
+        if language_st == 'english' or language_st == 'en':  # and score > 0.85:
             item['subtitle'] = subtitle
+        else:
+            item['subtitle'] = self.lp.ggl_translate(subtitle, language_st)
 
         # Extract most frequent words
         item['keywords'] = self.lp.get_keywords(item['storyText'])
